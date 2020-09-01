a0aaf06683ae8cc0f69b49b4479caeb7504f3c90
Alex <forfun@gmail.com>
2020-09-01 13:23:52 +0300

    Идеально выполненная работа

--- udemy.js
+++ udemy.js
@@ -1,43 +1,27 @@
 "use strict";
 
-let numberOfFilms;
-
-let a;
-
-let b;
-
 let personalMovieDB = {
 
-    count: numberOfFilms,
-
-    movies: {
-
-    },
-
-    actors: {
-
-    },
-
+    count: 0,
+    movies: {},
+    actors: {},
     genres: [],
-
     privat: false,
 
-    start: function start() {
-        numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов посмотрели?', '');
 
-        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-            numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов посмотрели?', '');
         }
     },
 
-
-
-    remember: function rememberMyFilms() {
+    rememberMyFilms: function () {
         for (let i = 0; i < 1; i++) {
 
-            a = prompt('Ласт фильм?');
+            let a = prompt('Ласт фильм?');
 
-            b = prompt('Оценка?');
+            let b = prompt('Оценка?');
 
             if (a != '' && b != '' && a != null && b != null & a.length < 10) {
                 personalMovieDB.movies[a] = b;
@@ -49,7 +33,7 @@ let personalMovieDB = {
         }
     },
 
-    detect: function detectPersonalLevel() {
+    detectPersonalLevel: function () {
         if (personalMovieDB.count < 10) {
             alert('Ты посмотрел слишком мало фильмов');
         } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
@@ -61,56 +45,37 @@ let personalMovieDB = {
         }
     },
 
-    show: function showMyDB() {
-        if (personalMovieDB.privat == false) {
+    showMyDB: function (hidden) {
+        if (!hidden) {
             console.log(personalMovieDB);
         } else {
             console.log('unlucky');
         }
     },
 
-    toggle: function toggleVisibleMyDB() {
-        if (personalMovieDB.privat == false) {
-            personalMovieDB.privat = true;
-        } else {
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
             personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
     },
-
-    write: function writeYourGenres() {
+    writeYourGenres: function () {
         for (let i = 1; i < 4; i++) {
 
             let genress = prompt(`Ваш любимый жанр под номером ${i}`);
-            personalMovieDB.genres[i - 1] = genress;
-
             if (genress == '' || genress == null) {
                 console.log('Еще разок');
                 i--;
-            } else if (i == 3) {
-                personalMovieDB.genres.forEach(function callback(current, index, array) {
-                    console.log(`Любимый жанр # ${index+1} - это ${current}`);
-                });
+            } else {
+                personalMovieDB.genres[i - 1] = genress;
+
             }
 
         }
+        personalMovieDB.genres.forEach(function (current, index) {
+            console.log(`Любимый жанр # ${index+1} - это ${current}`);
+        });
     },
 
 };
-
-let start = personalMovieDB.start;
-start();
-
-let remember = personalMovieDB.remember;
-remember();
-
-let detect = personalMovieDB.detect;
-detect();
-
-let show = personalMovieDB.show;
-show();
-
-let toggle = personalMovieDB.toggle;
-toggle();
-
-let write = personalMovieDB.write;
-write();
