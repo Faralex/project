8f5c8316c88829543d115bd83d471ea360d4461c
Alex <forfun@gmail.com>
2020-09-01 12:14:08 +0300

    Чувствую себя умным!! Здесь много ошибок, но норм

--- udemy.js
+++ udemy.js
@@ -6,16 +6,6 @@ let a;
 
 let b;
 
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
-    }
-}
-
-start();
-
 let personalMovieDB = {
 
     count: numberOfFilms,
@@ -31,57 +21,96 @@ let personalMovieDB = {
     genres: [],
 
     privat: false,
-};
 
-function rememberMyFilms() {
-    for (let i = 0; i < 1; i++) {
+    start: function start() {
+        numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
 
-        a = prompt('Ласт фильм?');
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
+        }
+    },
 
-        b = prompt('Оценка?');
 
-        if (a != '' && b != '' && a != null && b != null & a.length < 10) {
-            personalMovieDB.movies[a] = b;
-            console.log('gratz');
+
+    remember: function rememberMyFilms() {
+        for (let i = 0; i < 1; i++) {
+
+            a = prompt('Ласт фильм?');
+
+            b = prompt('Оценка?');
+
+            if (a != '' && b != '' && a != null && b != null & a.length < 10) {
+                personalMovieDB.movies[a] = b;
+                console.log('gratz');
+            } else {
+                i--;
+                console.log('unlucky');
+            }
+        }
+    },
+
+    detect: function detectPersonalLevel() {
+        if (personalMovieDB.count < 10) {
+            alert('Ты посмотрел слишком мало фильмов');
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+            alert('Вы дефолтный зритель');
+        } else if (personalMovieDB.count > 30) {
+            alert('Воу, да вы киноман.');
         } else {
-            i--;
+            alert('Произошла ошибка. Как вы это сделали?');
+        }
+    },
+
+    show: function showMyDB() {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        } else {
             console.log('unlucky');
         }
-    }
-}
+    },
 
-rememberMyFilms();
+    toggle: function toggleVisibleMyDB() {
+        if (personalMovieDB.privat == false) {
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
+        }
+    },
 
+    write: function writeYourGenres() {
+        for (let i = 1; i < 4; i++) {
 
+            let genress = prompt(`Ваш любимый жанр под номером ${i}`);
+            personalMovieDB.genres[i - 1] = genress;
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert('Ты посмотрел слишком мало фильмов');
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        alert('Вы дефолтный зритель');
-    } else if (personalMovieDB.count > 30) {
-        alert('Воу, да вы киноман.');
-    } else {
-        alert('Произошла ошибка. Как вы это сделали?');
-    }
-}
+            if (genress == '' || genress == null) {
+                console.log('Еще разок');
+                i--;
+            } else if (i == 3) {
+                personalMovieDB.genres.forEach(function callback(current, index, array) {
+                    console.log(`Любимый жанр # ${index+1} - это ${current}`);
+                });
+            }
 
-detectPersonalLevel();
+        }
+    },
 
-function showMyDB() {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    } else {
-        console.log('unlucky');
-    }
-}
-showMyDB();
+};
 
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++) {
-        let genress = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres.push(genress);
-    }
-}
+let start = personalMovieDB.start;
+start();
 
-writeYourGenres();
+let remember = personalMovieDB.remember;
+remember();
+
+let detect = personalMovieDB.detect;
+detect();
+
+let show = personalMovieDB.show;
+show();
+
+let toggle = personalMovieDB.toggle;
+toggle();
+
+let write = personalMovieDB.write;
+write();
